5eafdae7721b2f148827d19bc06a0c57a3ec315e
TsujiKeisuke <ltksk0714@gmail.com>
2022-07-27 22:32:30 +0900

    add: ReactLocationを入れて、適当なページ遷移を作成

--- frontend/src/components/TodoPage.tsx
+++ frontend/src/components/TodoPage.tsx
@@ -1,3 +1,3 @@
 export const  TodoPage = () => {
-  <div>TodoPage</div>
+  return <div>TodoPage</div>
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ReactLocation, Router } from "@tanstack/react-location";
+import { TodoPage } from "./components/TodoPage";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const location = new ReactLocation();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router
+      location={location}
+      routes={[
+        { path: "/", element: <App /> },
+        { path: "todo", element: <TodoPage /> },
+      ]}
+    ></Router>
   </React.StrictMode>
-)
+);
