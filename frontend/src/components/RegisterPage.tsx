6320d0df9668526f60a9ae62109051c78cab3e18
TsujiKeisuke <ltksk0714@gmail.com>
2022-08-03 22:28:59 +0900

    add: submitでregisterを呼ぶようにした

--- frontend/src/components/RegisterPage.tsx
+++ frontend/src/components/RegisterPage.tsx
@@ -1,6 +1,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 
 type Input = {
+  name: string;
   email: string;
   password: string;
 };
@@ -15,14 +16,50 @@ export const RegisterPage = () => {
     formState: { errors },
   } = useForm<Input>();
 
-  const onSubmit: SubmitHandler<Input> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<Input> = (data) => {
+    console.log(data);
+    fetch("http://127.0.0.1:8000/register", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((resp) => console.log(resp))
+      .catch((err) => console.error(err));
+  };
 
   return (
     <div className="lg:container shadow-md flex flex-col px-8 pt-6 pb-8 bg-white mx-auto text-left rounded-xl shadow-slate-400 ">
       <h1 className="text-4xl mb-4">新規登録</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <label className="block font-bold mb-2 text-sm">Email address</label>
+          <label>Name</label>
+          <input
+            className="shadow borderd rounded appearance-none w-full py-2 px-3"
+            placeholder="enter your name"
+            type="text"
+            defaultValue=""
+            {...register("name", {
+              required: true,
+              maxLength: 32,
+              minLength: 4,
+            })}
+          />
+        </div>
+        <p className="text-red-600">{errors.name && "nameは必須です"}</p>
+        <p className="text-red-600">
+          {errors.name?.type === "maxLength" &&
+            "nameは32文字以下である必要があります"}
+        </p>
+        <p className="text-red-600">
+          {errors.name?.type === "minLength" &&
+            "nameは4文字以上である必要があります"}
+        </p>
+
+        <div className="mb-4">
+          <label>Email address</label>
           <input
             className="shadow borderd rounded appearance-none w-full py-2 px-3"
             placeholder="enter your email address"
@@ -35,17 +72,20 @@ export const RegisterPage = () => {
           />
         </div>
         <p className="text-red-600">{errors.email && "emailは必須です"}</p>
-        <label>Password</label>
-        <input
-          className="shadow borderd rounded appearance-none w-full py-2 px-3"
-          placeholder="enter your password"
-          type="password"
-          {...register("password", {
-            required: true,
-            maxLength: 64,
-            minLength: 16,
-          })}
-        />
+
+        <div className="mb4">
+          <label>Password</label>
+          <input
+            className="shadow borderd rounded appearance-none w-full py-2 px-3"
+            placeholder="enter your password"
+            type="password"
+            {...register("password", {
+              required: true,
+              maxLength: 64,
+              minLength: 16,
+            })}
+          />
+        </div>
         <p className="text-red-600">
           {errors.password && "passwordは必須です"}
         </p>
