055ffbb31fc639191c38a746fb5383d329136294
TsujiKeisuke <ltksk0714@gmail.com>
2022-08-17 22:01:34 +0900

    fix: AuthGuardの認証確認中の表示を追加

--- frontend/src/components/AuthGuard.tsx
+++ frontend/src/components/AuthGuard.tsx
@@ -3,21 +3,27 @@ import { useQuery } from "@tanstack/react-query";
 import { Navigate } from "@tanstack/react-location";
 
 export function AuthGuard(props: { children: ReactElement }) {
-  const { data } = useQuery(["user"], () =>
-    fetch("http://localhost:8000/api/user", {
-      mode: "cors",
-      credentials: "include",
-    })
-      .then((resp) => {
-        if (!resp.ok) throw new Error("userの取得に失敗しました");
-        return resp.json();
+  const { data, isLoading } = useQuery(
+    ["user"],
+    () =>
+      fetch("http://localhost:8000/api/user", {
+        mode: "cors",
+        credentials: "include",
       })
-      .then((resp) => {
-        return resp.data;
-      })
+        .then((resp) => {
+          if (!resp.ok) throw new Error("userの取得に失敗しました");
+          return resp.json();
+        })
+        .then((resp) => {
+          return resp.data;
+        }),
+    {
+      retry: false,
+    }
   );
-  const isLoggedIn = !!data;
+  if (isLoading) return <div>認証情報確認中</div>;
 
+  const isLoggedIn = !!data;
   if (!isLoggedIn) return <Navigate to="/login" />;
   return props.children;
 }
